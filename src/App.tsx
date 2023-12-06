7f84abccfe0348e3aab711a3ecefd5eb03a09c3b
netcam22 <annettelesage@btinternet.com>
2023-12-06 08:26:03 +0000

    feat: remove html cards

--- src/App.tsx
+++ src/App.tsx
@@ -13,25 +13,7 @@ function App(): JSX.Element {
 			<Header />
 
 			<main>
-				<div className='cards__wrapper'>
-					
-					<div className="card">
-						<h3 className="card__text card__header">Cindy Clawford</h3>
-						<img className="card__image" src="someImageUrlHere" alt="A cute tabby kitten"></img>
-						<p className="card__text">Species: Cat</p>
-						<p className="card__text">Favourite Food(s): Mice</p>
-						<p className="card__text">Birth Year: 2012</p>
-					</div>
-
-					<div className="card">
-						<h3 className="card__text card__header">Little Miss Purrfect</h3>
-						<img className="card__image" src="someImageUrlHere" alt="A gray fluffy kitten"></img>
-						<p className="card__text">Species: Cat</p>
-						<p className="card__text">Favourite Food(s): Wet food, dry food</p>
-						<p className="card__text">Birth Year: 2016</p>
-					</div>
-
-				</div>
+				<div className='cards__wrapper'>{/* JSX code can go here */}</div>
 			</main>
 
 			<Footer />
