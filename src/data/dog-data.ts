d45a2bdae56f7cf0174fba74b1d1e03b5217ffcd
netcam22 <annettelesage@btinternet.com>
2023-12-12 16:57:59 +0000

    feat: add creative commons dog images and attributions

--- src/data/dog-data.ts
+++ src/data/dog-data.ts
@@ -1,27 +1,27 @@
 import { v4 as uuidv4 } from 'uuid';
 import {Pet, PetInfo} from './pet';
-import dog1 from '../assets/images/cat1.jpg';
-import dog2 from '../assets/images/cat2.jpg';
-import dog3 from '../assets/images/cat3.jpg';
+import dog1 from '../assets/images/dog1.jpg';
+import dog2 from '../assets/images/dog2.jpg';
+import dog3 from '../assets/images/dog3.jpg';
 
 const dogs: Array<Pet> = [
 	{
 		name: 'Floofus',
 		species: 'Puppy',
 		favFoods: ['marshmallows'],
-		birthYear: 2020,
+		birthYear: 2022,
 	},
 	{
 		name: 'Doofus',
 		species: 'Doggo',
 		favFoods: ['porridge, dog food'],
-		birthYear: 2010,
+		birthYear: 2022,
 	},
 	{
 		name: 'Goofus',
 		species: 'Dog',
 		favFoods: ['bouncy balls'],
-		birthYear: 2015,
+		birthYear: 2018,
 	},
 ];
 
@@ -29,26 +29,26 @@ const dogImages = [
 	{
 		image: dog1,
 		altText: 'Describe this dog!',
-		licenceType: 'CC BY-SA 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'amblin',
-		attributionUrl: 'https://www.flickr.com/people/amblin/'
+		licenceType: 'CC BY-SA 3.0',
+		licenceUrl: 'https://creativecommons.org/licenses/by-sa/3.0/',
+		attributionName: 'Filip B',
+		attributionUrl: 'https://commons.wikimedia.org/wiki/File:Dog_In_The_Grass.jpg'
 	},
 	{
 		image: dog2,
 		altText: 'Describe this dog!',
 		licenceType: 'CC BY-SA 2.0',
 		licenceUrl: 'https://creativecommons.org/licenses/by-sa/2.0/',
-		attributionName: 'ivva',
-		attributionUrl: 'https://www.flickr.com/people/ivva/'
+		attributionName: 'leisergu',
+		attributionUrl: 'https://www.flickr.com/photos/leisergu/2873249326'
 	},
 	{
 		image: dog3,
 		altText: 'Describe this dog!',
-		licenceType: 'CC BY-ND 2.0',
-		licenceUrl: 'https://creativecommons.org/licenses/by-nd/2.0/',
-		attributionName: 'Rachele Pettarelli Ph',
-		attributionUrl: 'https://www.flickr.com/people/rachephotos/'
+		licenceType: 'CC BY-SA 3.0',
+		licenceUrl: 'https://creativecommons.org/licenses/by-sa/3.0',
+		attributionName: 'Sp..andreea        ',
+		attributionUrl: 'https://commons.wikimedia.org/wiki/File:My_dog.jpg'
 	},
 ];
 
@@ -62,7 +62,6 @@ export const dogData = dogs.map((dog: Pet, index: number): PetInfo => {
 		attributionName: dogImages[index].attributionName,
 		attributionUrl: dogImages[index].attributionUrl};
 });
-
 
 export const dogEndPoint = "https://api.thedogapi.com/v1/images/search?limit=6&has_breeds=1&api_key=live_SGCOCUSRlMPtuUyfbvJYyO3b54YDIH50fQWGPwY8yySWMudUobwONZkbMBiWUzBZ";
 export const dogNames = ["Milo", "Charlie", "Teddy", "Luna", "Coco", "Bella"];
